664eb4bdd52900efef2d89fcd021187e381e9ba7
CCXXXI <ccxxxi@qq.com>
2020-07-17 16:05:08 +0800

    Create config.example.py

---
+++ config.example.py
@@ -0,0 +1,18 @@
+SUPERUSERS = [12345]
+HOST = '127.0.0.1'
+PORT = 8080
+COMMAND_START = {''}
+
+group_white_list = {
+    12345,  # 示例
+}
+
+user_white_list = {
+    12345,  # 示例
+}
+
+word_cloud_groups = {
+    12345: "文本",  # 示例
+}
+
+dalao_delta_groups = group_white_list.copy()
