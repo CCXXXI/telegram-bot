4ef750186dd6bf34e92256c0e3801f65c926fa75
CCXXXI <ccxxxi@qq.com>
2020-07-18 19:44:51 +0800

    rename

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 
 以下内容被ignore，但缺少则不可运行
 
-* `img_daily/`：放置每日图片
+* `img_daily/` `img_gkd/`：放置图片
 * `word_cloud/`：字体文件`font.ttf`、屏蔽词`stopwords.txt`、词云模板`template.png`
 * `config.py`：参考[config.example.py](./config.example.py)
--- plugins/gkd.py
+++ plugins/gkd.py
@@ -12,11 +12,8 @@ async def gkd_forward(session: CommandSession):
     if session.event.group_id in group_white_list or session.event.user_id in user_white_list:
         img_name = choice(listdir('img_gkd'))
         img_path = getcwd() + rf'\img_gkd\{img_name}'
+        print(f'{img_path=}')
         msg = f'来啦\n[CQ:image,file=file:///{img_path}]'
         await session.send(msg)
-        await asyncio.sleep(10)
-        await session.send('再来一张')
-        await asyncio.sleep(2)
-        await session.send('/不色的图')
     else:
         print('来源不明，pass')
