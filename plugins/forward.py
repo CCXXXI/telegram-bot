4c88c36c7f708db0e366474a806a4d4a4fdc7105
CCXXXI <ccxxxi@qq.com>
2020-07-18 19:30:24 +0800

    rename

--- plugins/forward.py
+++ plugins/forward.py
@@ -10,8 +10,8 @@ from config import group_white_list, user_white_list
 @on_command('gkd')
 async def gkd_forward(session: CommandSession):
     if session.event.group_id in group_white_list or session.event.user_id in user_white_list:
-        img_name = choice(listdir('img_test'))
-        img_path = getcwd() + rf'\img_test\{img_name}'
+        img_name = choice(listdir('img_gkd'))
+        img_path = getcwd() + rf'\img_gkd\{img_name}'
         msg = f'来啦\n[CQ:image,file=file:///{img_path}]'
         await session.send(msg)
         await asyncio.sleep(10)
