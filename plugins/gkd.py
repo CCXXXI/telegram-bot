a0a55fadd09c85409569ff52fc98651d06fb3301
CCXXXI <ccxxxi@qq.com>
2020-07-18 20:24:15 +0800

    Update gkd.py

--- plugins/gkd.py
+++ plugins/gkd.py
@@ -13,7 +13,11 @@ async def gkd_forward(session: CommandSession):
         img_name = choice(listdir('img_gkd'))
         img_path = getcwd() + rf'\img_gkd\{img_name}'
         print(f'{img_path=}')
-        msg = f'来啦\n[CQ:image,file=file:///{img_path}]'
+        msg = f'[CQ:image,file=file:///{img_path}]'
         await session.send(msg)
+        await asyncio.sleep(10)
+        await session.send('再来一张')
+        await asyncio.sleep(2)
+        await session.send('/不色的图')
     else:
         print('来源不明，pass')
