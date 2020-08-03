69282aea51d357383358d95cc67594e37ef8b4a4
CCXXXI <ccxxxi@qq.com>
2020-08-03 20:55:01 +0800

    start

---
+++ plugins/start.py
@@ -0,0 +1,10 @@
+from telegram import Update
+from telegram.ext import CallbackContext
+
+from plugin_tools import on_cmd
+
+
+# noinspection PyUnusedLocal
+@on_cmd
+def start_(update: Update, context: CallbackContext):
+    update.message.reply_text('Started!')
