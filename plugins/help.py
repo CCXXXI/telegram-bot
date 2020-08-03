49d137013c3eed44ba60b09ee20ea6dac9371383
CCXXXI <ccxxxi@qq.com>
2020-08-03 18:21:50 +0800

    help

---
+++ plugins/help.py
@@ -0,0 +1,10 @@
+from telegram import Update
+from telegram.ext import CallbackContext
+
+from plugin_tools import on_cmd
+
+
+# noinspection PyUnusedLocal
+@on_cmd
+def help_(update: Update, context: CallbackContext):
+    update.message.reply_text('https://github.com/CCXXXI/telegram_bot')
