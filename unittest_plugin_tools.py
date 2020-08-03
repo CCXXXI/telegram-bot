aaa392a20eaccdc96f8ed4131f071377b7b0ea20
CCXXXI <ccxxxi@qq.com>
2020-08-03 15:10:34 +0800

    add unittest for plugin_tools

---
+++ unittest_plugin_tools.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestPluginTools(unittest.TestCase):
+    def test_plugin_tools(self):
+        import plugin_tools
+        plugin_tools.load_plugins()
+        for cmd in plugin_tools.cmd_list:
+            print(*cmd.command)
+        self.assertTrue(plugin_tools.cmd_list)
+
+
+if __name__ == '__main__':
+    unittest.main()
