8a062298c43f642b12c149a0e7ae3b3932b1f9fe
CCXXXI <ccxxxi@qq.com>
2020-08-03 17:59:39 +0800

    get -> post

--- plugins/exec.py
+++ plugins/exec.py
@@ -17,7 +17,7 @@ def exec_(update: Update, context: CallbackContext):
 
 
 def safe_exec(s: str) -> str:
-    ret = requests.get(safe_exec_api + s)
+    ret = requests.post(safe_exec_api, data=s)
     if ret.status_code == 200 and ret.text:
         return ret.text
     return '?'
