1ca54f8b0254c31c74f3a3c6173311695ffc9d1e
CCXXXI <ccxxxi@qq.com>
2020-08-03 16:34:39 +0800

    logging

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import logging
+
 from telegram.ext import Updater
 
 import plugin_tools
 from config import token
 
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 updater = Updater(
     token=token,
     use_context=True)  # `use_context=True` only needed for version 12
@@ -11,9 +15,9 @@ plugin_tools.load_plugins()
 
 for cmd in plugin_tools.cmd_list:
     updater.dispatcher.add_handler(cmd)
-    print('command added:', cmd.command)
+    logging.info(f'command added: {cmd.command}')
 
 if __name__ == '__main__':
     updater.start_polling()
-    print('updater running')
+    logging.info('updater running')
     updater.idle()
