cc8a7830290d976d30a740659a6126681a220084
CCXXXI <ccxxxi@qq.com>
2020-08-28 01:58:42 +0800

    Update main.py

--- web/main.py
+++ web/main.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 
 @app.route('/ecnu')
 def ecnu_faq():
-    faq_list = json.load(open('ecnu_faq.json'))
+    faq_list = json.load(open('ecnu_faq.json', encoding='utf-8'))
     for faq in faq_list:
         faq[1] = md(faq[1])
     return render_template('ecnu_faq.html', faqs=faq_list)
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 80, debug=False)
+    app.run(debug=True)
