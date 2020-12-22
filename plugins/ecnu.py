3a17f2b1801d4ad1499a23f11bbc3e817aae10ad
CCXXXI <ccxxxi@qq.com>
2020-12-22 20:52:32 +0800

    调整意愿值公式

--- plugins/ecnu.py
+++ plugins/ecnu.py
@@ -18,9 +18,13 @@ def ecnu_(update: Update, context: CallbackContext):
         res = '?'
     else:
         a, b = map(int, ab)
-        res = str(ceil(f(a, b)))
+        res = str(f(a, b))
     update.message.reply_text(res)
 
 
 def f(a, b):
-    return 0 if a < b else 5 * (2 * a / b - 1) * log(a - b)
+    if a <= b:
+        return 0
+    if b == 0:
+        return '?'
+    return ceil(5 * (2 * a / b - 1) * log(a - b + 1))
